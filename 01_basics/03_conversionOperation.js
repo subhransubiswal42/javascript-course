e5c01da59be5139d2326df5a96f9add39cc84b5d
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-02 14:35:08 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+ let score = "Subhransu"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber); //NaN
+
+
+/*"33" => 33
+"33abc" => NaN
+true => 1; false => 0 */
+
+let isLoggedIn = "Sinu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+/* 1 => true
+ 0 => false
+ "" => false
+ "sinu" => true
+ null => false
+ undefined => false
+ " " => true */
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

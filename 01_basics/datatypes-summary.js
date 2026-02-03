a0832f551e6c6fc24331f00fce8051fff50f548d
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-03 14:47:30 +0000

    Stack&Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "subhransudotcom"
+
+let anothername = myYoutubename
+anothername = "sinu"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@gmail.com"
+    // upi: 'user@ybl'
+}
+
+let userTwo = userOne
+
+userTwo.email = "subhransu@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+

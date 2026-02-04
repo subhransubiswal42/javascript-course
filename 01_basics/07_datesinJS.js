9b4cc9d4cf8b2952d699c369b4c25fbc3bdad510
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-04 06:36:59 +0000

    Learn - Dates

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,35 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toJSON());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026, 0, 23)
+// let myCreatedDate = new Date(2026, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2026-01-14")
+let myCreatedDate = new Date("01-14-2026")
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/100));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+
+//Best way to define date or time
+console.log(newDate.toLocaleString('default',{
+    weekday: "long",
+}))

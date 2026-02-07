06b7fa441decc83ea1787d9a1aaf2735ebfc35ed
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-07 15:25:36 +0000

    Learn - Objects(part - 2)

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+
+const regularUser = {
+    email: "some@email.com",
+    fullname: {
+        userfullname: {
+            firstname: "Subhransu",
+            lastname: "Biswal"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2} // This is the way to merge two objects in JavaScript using spread operator
+// const obj3 = Object.assign({}, obj1, obj2) // This is another way to merge two objects in JavaScript using Object.assign() method
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3); // This will print the merged object with all the key-value pairs of both the objects
+
+
+// from the database value
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+
+    {
+        id: 2,
+        email: "ss@gmail.com"
+    }
+]
+
+console.log(users[0].email)
+
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // This will return true because the object has the property 'isLoggedIn'
+console.log(tinderUser.hasOwnProperty('isLogged')); // This will return false because the object does not have the property 'isLogged' 

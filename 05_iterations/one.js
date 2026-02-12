9036345fb4be511a0edc995e2fd9512f8b1797c6
Subhransu Biswal <subhransubiswal423@gmail.com>
2026-02-12 05:22:49 +0000

    Discription

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -15,7 +15,7 @@ for (let i = 1; i <= 10; i++) {
     //console.log(`Outer loop value: ${i}`);
    for (let j = 1; j <= 10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
-    //console.log(i + '*' + j + ' = ' + i*j );
+    //console.log(i + '*' + j + ' = ' + i*j );  //it will print the multiplication table of 1 to 10
    }
     
 }
